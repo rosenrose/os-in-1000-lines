295f802a04c1fa743bf3529738fbe0f8140fddc1
rosenrose <crazytempler@gmail.com>
2025-02-04 17:37:50 +0900

    boot

--- run.sh
+++ run.sh
@@ -2,5 +2,9 @@
 set -xue
 
 QEMU=qemu-system-riscv32
+CC=/usr/bin/clang
+CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32-unknown-elf -fno-stack-protector -ffreestanding -nostdlib"
 
-$QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot
+$CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf kernel.c
+
+$QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot -kernel kernel.elf
